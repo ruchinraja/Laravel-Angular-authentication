e73f3648d1d596c15a1a2896d65850b570a45be9
ruchin <ruchinchauhan180@gmail.com>
2018-12-26 01:05:37 +0530

    Angular Routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import {ProfileComponent} from './components/profile/profile.component';
+import { ResetRequestComponent } from './components/passwords/reset-request/reset-request.component';
+import { ResponseRequestComponent } from './components/passwords/response-request/response-request.component';
+import { Profile } from 'selenium-webdriver/firefox';
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'signup',
+    component: SignupComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  },
+  {
+    path: 'request-password-reset',
+    component: ResetRequestComponent
+  },
+  {
+    path: 'response-password-reset',
+    component: ResponseRequestComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
